5b2059c470ab7a53bd658c21fa47c29baac66eeb
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-01-12 19:34:57 -0300

    Corrigido conn + criada controllers e rotas de user

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -5,6 +5,6 @@ async function main() {
     console.log("Conectado ao Banco de Dados");
 }
 
-main().catch((err = console.log(err)));
+main().catch((err) => console.log(err));
 
 module.exports = mongoose;
