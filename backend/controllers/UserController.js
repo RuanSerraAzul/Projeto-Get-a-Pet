395cb4c3d7fad831ab1ecb1757d72001c94baa7e
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-01-14 10:07:36 -0300

    adicionado middleware de verificação de token de usuario

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -157,4 +157,12 @@ module.exports = class UserController {
 
         res.status(200).json({ user });
     }
+
+    static async editUser(req, res) {
+        res.status(200).json({
+            message: "Deu certo o update",
+        });
+
+        return;
+    }
 };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 
 const UserController = require("../controllers/UserController");
 
+//middlewares
+const verifyToken = require("../helpers/verify-token");
+
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.get("/checkUser", UserController.checkUser);
+router.patch("/edit/:id", verifyToken, UserController.editUser);
 router.get("/:id", UserController.getUserById);
 
 module.exports = router;
