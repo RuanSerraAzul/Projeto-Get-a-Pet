35cbead05db6ddd3a7e4f501e71f7f7804a864ca
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-01-18 17:57:33 -0300

    Estilização Navbar e Footer

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -2,20 +2,24 @@ import { Link } from "react-router-dom";
 
 import Logo from "../../assets/img/logo.png";
 
+import styles from "./Navbar.module.css";
+
 function Navbar() {
     return (
-        <nav>
-            <div>
+        <nav className={styles.navbar}>
+            <div className={styles.navbar_logo}>
                 <img src={Logo} alt="Get a Pet" />
             </div>
             <ul>
-                <Link to="/">Adotar</Link>
-            </ul>
-            <ul>
-                <Link to="/login">Entrar</Link>
-            </ul>
-            <ul>
-                <Link to="/register">Cadastrar</Link>
+                <li>
+                    <Link to="/">Adotar</Link>
+                </li>
+                <li>
+                    <Link to="/login">Entrar</Link>
+                </li>
+                <li>
+                    <Link to="/register">Cadastrar</Link>
+                </li>
             </ul>
         </nav>
     );
