9cb856fd312b2112cae1fe1a3eb2535cd252aa72
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-01-19 14:10:51 -0300

    Criado estilização do formulario de registros

--- frontend/src/components/pages/auth/Register.js
+++ frontend/src/components/pages/auth/Register.js
@@ -1,10 +1,12 @@
 import Input from "../../form/Input";
 
+import styles from "../../form/Form.module.css";
+import { Link } from "react-router-dom";
 function Register() {
     function handleChange(e) {}
 
     return (
-        <section>
+        <section className={styles.form_container}>
             <h1>Registrar</h1>
             <form>
                 <Input
@@ -49,6 +51,9 @@ function Register() {
 
                 <input type="submit" value="Cadastrar" />
             </form>
+            <p>
+                Já tem conta? <Link to="/login">Clique aqui.</Link>
+            </p>
         </section>
     );
 }
