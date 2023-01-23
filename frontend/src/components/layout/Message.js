ddb671584ebd382ac7f67dfc6f48579c9e4d3696
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-01-23 08:39:54 -0300

    Finalizando componente de flash message

--- frontend/src/components/layout/Message.js
+++ frontend/src/components/layout/Message.js
@@ -1,13 +1,28 @@
 import styles from "./Message.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import bus from "../../utils/bus";
 
 function Message() {
+    const [visibillity, setVisibility] = useState(false);
+    const [message, setMessage] = useState("");
     const [type, setType] = useState("");
 
+    useEffect(() => {
+        bus.addListener("flash", ({ message, type }) => {
+            setVisibility(true);
+            setMessage(message);
+            setType(type);
+
+            setTimeout(() => {
+                setVisibility(false);
+            }, 4000);
+        });
+    }, []);
+
     return (
-        <div className={`${styles.message} ${styles[type]}`}>
-            Minha mensagem
-        </div>
+        visibillity && (
+            <div className={`${styles.message} ${styles[type]}`}>{message}</div>
+        )
     );
 }
 
--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -2,9 +2,15 @@ import api from "../utils/api";
 
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import useFlashMessage from "./useFlashMessage";
 
 export default function useAuth() {
+    const { setFlashMessage } = useFlashMessage();
+
     async function register(user) {
+        let msgText = "Cadastro realizado com sucesso";
+        let msgType = "success";
+
         try {
             const data = await api
                 .post("/users/register", user)
@@ -14,8 +20,12 @@ export default function useAuth() {
 
             console.log(data);
         } catch (error) {
-            console.log(error);
+            // tratar erro
+            msgText = error.response.data.message;
+            msgType = "error";
         }
+
+        setFlashMessage(msgText, msgType);
     }
 
     return { register };
