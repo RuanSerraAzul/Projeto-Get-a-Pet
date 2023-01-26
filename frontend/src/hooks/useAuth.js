6266e466d569e3a3af261ed768319733835b5b99
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-01-25 22:05:06 -0300

    Função de Login

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -46,6 +46,26 @@ export default function useAuth() {
 
         history("/");
     }
+    async function login(user) {
+        let msgText = "Login realizado com sucesso!";
+        let msgType = "success";
+
+        try {
+            const data = await api
+                .post("/users/login", user)
+                .then((response) => {
+                    return response.data;
+                });
+
+            await authUser(data);
+        } catch (error) {
+            // tratar erro
+            msgText = error.response.data.message;
+            msgType = "error";
+        }
+
+        setFlashMessage(msgText, msgType);
+    }
 
     function logout() {
         const msgText = "Logout realizado com sucesso";
@@ -60,5 +80,5 @@ export default function useAuth() {
         setFlashMessage(msgText, msgType);
     }
 
-    return { authenticated, register, logout };
+    return { authenticated, register, logout, login };
 }
